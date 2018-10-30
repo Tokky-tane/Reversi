d00998681caac833cbde7a20da54f01bddf0e4b5
Tokky_tane <mitokinksa-dos3zero@outlook.jp>
2018-10-30 22:30:42 +0900

    左斜め上の処理を実装

--- main.cpp
+++ main.cpp
@@ -225,6 +225,23 @@ void change_board(int player_frame, int x, int y, int board[N][N]) {
     }
     X = x;
     Y = y;
+
+    // 左斜め上
+    while (board[X - 1][Y - 1] == rival_frame) {
+        X--;
+        Y--;
+    }
+    if (board[X - 1][Y - 1] == player_frame) {
+        X = x;
+        Y = y;
+        while (board[X - 1][Y - 1] == rival_frame) {
+            board[X - 1][Y - 1] = player_frame;
+            X--;
+            Y--;
+        }
+    }
+    X = x;
+    Y = y;
 }
 
 int check_change(int player_frame, int x, int y, int board[N][N]) {
@@ -312,6 +329,19 @@ int check_change(int player_frame, int x, int y, int board[N][N]) {
     X = x;
     Y = y;
 
+    // 左斜め上
+    while (board[X - 1][Y - 1] == rival_frame) {
+        check = 1;
+        X--;
+        Y--;
+    }
+    if (board[X - 1][Y - 1] == player_frame && check == 1) {
+        check = 1;
+        return check;
+    }
+    check = 0;
+    X = x;
+    Y = y;
 
     return 0;
 }
